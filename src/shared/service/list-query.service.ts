06ec65ca55610f31777dd25dc40f1590ba0a39a0
mikasa <onlymisaky@gmail.com>
2024-12-26 19:05:01 +0800

    feat: listQueryService 支持灵活查询
    - 引入了一种新的 handleQueryBuilder 方法来管理动态查询参数和排序选项。
    - 增加了对各种查询类型的支持: 'exact' | 'like' | 'in' | 'between'
    - 更新 createListQueryBuilder，以接受 queryBuilderOptions，从而改进可配置性。
    - getPagedList，返回 list

--- src/shared/service/list-query.service.ts
+++ src/shared/service/list-query.service.ts
@@ -2,26 +2,111 @@ import { Injectable } from '@nestjs/common';
 import { Repository, SelectQueryBuilder } from 'typeorm';
 import { ListQueryDto } from '../dto/list-quert.dto';
 
+interface QueryConfig {
+  [key: string]: {
+    dbField?: string
+    queryType?: 'exact' | 'like' | 'in' | 'between'
+  }
+}
+
+interface Builder<Entity> { builder: SelectQueryBuilder<Entity> }
+interface Alias { alias: string }
+type QueryBuilderOptions<Entity> = Partial<Builder<Entity> | Alias> & { queryConfig?: QueryConfig };
+
 @Injectable()
 export class ListQueryService {
-  createListQueryBuilder<Entity>(
-    repository: Repository<Entity>,
-    alias: string,
-    queryDto: ListQueryDto,
-    queryBuilder?: SelectQueryBuilder<Entity>,
-  ): SelectQueryBuilder<Entity> {
-    const builder = queryBuilder || repository.createQueryBuilder(alias);
-    const { params = {}, sort } = queryDto;
+  protected handleQueryBuilder(
+    queryBuilder: SelectQueryBuilder<any>,
+    params: Record<string, any>,
+    queryConfig: QueryConfig = {},
+    sort?: { sortBy: string, sortOrder: 'ASC' | 'DESC' },
+  ) {
+    const alias = queryBuilder.alias;
 
-    // 处理查询参数
-    Object.keys(params).forEach(key => {
-      builder.andWhere(`${alias}.${key} = :${key}`, { [key]: params[key] });
+    Object.keys(params).forEach(paramKey => {
+      const value = params[paramKey];
+
+      if (value === undefined || value === '' || value === null)
+        return;
+
+      let values: Array<string | number | any> = [];
+      if (Array.isArray(value)) {
+        values = value;
+      }
+      else if (typeof value === 'string') {
+        values = value.split(',');
+      }
+      else {
+        values = [value] as any[];
+      }
+
+      const config = queryConfig[paramKey] || { dbField: paramKey, queryType: 'like' };
+      const { dbField = paramKey, queryType = 'like' } = config;
+
+      switch (queryType) {
+        case 'like':
+          queryBuilder.andWhere(`${alias}.${dbField} LIKE :${paramKey}`, {
+            [paramKey]: `%${value}%`,
+          });
+          break;
+        case 'in':
+          queryBuilder.andWhere(`${alias}.${dbField} IN (:...${paramKey})`, {
+            [paramKey]: values,
+          });
+          break;
+        case 'between':
+          if (values.length === 2) {
+            queryBuilder.andWhere(`${alias}.${dbField} BETWEEN :${paramKey}Start AND :${paramKey}End`, {
+              [`${paramKey}Start`]: values[0],
+              [`${paramKey}End`]: values[1],
+            });
+          }
+          break;
+        case 'exact':
+          queryBuilder.andWhere(`${alias}.${dbField} = :${paramKey}`, {
+            [paramKey]: value,
+          });
+      }
     });
 
-    // 处理排序
-    if (sort) {
-      builder.orderBy(`${alias}.${sort.sortBy}`, sort.sortOrder);
+    if (sort?.sortBy) {
+      const sortConfig = queryConfig[sort.sortBy] || {};
+      const sortField = sortConfig.dbField || sort.sortBy;
+      queryBuilder.orderBy(`${alias}.${sortField}`, sort.sortOrder);
     }
+  }
+
+  createListQueryBuilder<Entity>(
+    repository: Repository<Entity>,
+    queryDto: ListQueryDto,
+    queryBuilderOptions?: QueryBuilderOptions<Entity>,
+  ): SelectQueryBuilder<Entity> {
+    let alias: string = (queryBuilderOptions as Alias)?.alias || 'entity';
+    alias = `${alias}`;
+    let builder: SelectQueryBuilder<Entity>;
+    let queryConfig: QueryConfig = {};
+
+    // 校验 + 赋值
+    // 可读性和代码简介性平衡
+    if (queryBuilderOptions) {
+      if ((queryBuilderOptions as Builder<Entity>).builder instanceof SelectQueryBuilder) {
+        builder = (queryBuilderOptions as Builder<Entity>).builder;
+      }
+      else {
+        builder = repository.createQueryBuilder(alias);
+      }
+
+      if (typeof queryBuilderOptions.queryConfig === 'object' && queryBuilderOptions.queryConfig !== null) {
+        queryConfig = queryBuilderOptions.queryConfig;
+      }
+    }
+    else {
+      builder = repository.createQueryBuilder(alias);
+    }
+
+    const { params = {}, sort } = queryDto;
+
+    this.handleQueryBuilder(builder, params, queryConfig, sort);
 
     return builder;
   }
@@ -29,13 +114,13 @@ export class ListQueryService {
   async getPagedList<Entity>(
     repository: Repository<Entity>,
     queryDto: ListQueryDto,
-    alias: string = 'entity',
+    queryOptions?: QueryBuilderOptions<Entity>,
   ) {
     const { pagination = { page: 1, pageSize: 10 } } = queryDto;
     const { page = 1, pageSize = 10 } = pagination;
     const skip = (page - 1) * pageSize;
 
-    const queryBuilder = this.createListQueryBuilder(repository, alias, queryDto);
+    const queryBuilder = this.createListQueryBuilder(repository, queryDto, queryOptions);
 
     const [items, total] = await queryBuilder
       .skip(skip)
@@ -43,7 +128,7 @@ export class ListQueryService {
       .getManyAndCount();
 
     return {
-      items,
+      list: items,
       pagination: {
         page: Number(page),
         pageSize: Number(pageSize),
