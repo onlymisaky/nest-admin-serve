20959d9ce989e63962b2e3d5bf9954e69dbff904
mikasa <onlymisaky@gmail.com>
2025-01-23 16:13:06 +0800

    feat: 添加手机号验证装饰器 IsMobile

---
+++ src/shared/decorators/validator/is-mobile.decorator.ts
@@ -0,0 +1,24 @@
+import { registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator';
+
+export function IsMobile(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isMobile',
+      target: object.constructor,
+      propertyName,
+      options: {
+        message: '请输入正确的手机号码',
+        ...validationOptions,
+      },
+      validator: {
+        validate(value: any, _args: ValidationArguments) {
+          if (typeof value !== 'string')
+            return false;
+          // 中国大陆手机号码正则表达式
+          // 支持 13x, 14x, 15x, 16x, 17x, 18x, 19x 开头的手机号
+          return /^1[3-9]\d{9}$/.test(value);
+        },
+      },
+    });
+  };
+}
